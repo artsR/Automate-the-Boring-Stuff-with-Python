f61ecb232bd3f1eb0d6f99e545797a51d0eaa80d
artsR <10280256+artsR@users.noreply.github.com>
2019-08-31 09:08:45 +0300

    Add files via upload

---
+++ Chapter 9 - Project - Renaming Files.py
@@ -0,0 +1,40 @@
+#! python3
+# renamesDates.py - Renames filenames with American MM-DD-YYYY date format
+# to European DD-MM-YYYY
+
+import shutil, os, re
+
+# Create a regex that matches files with the American date format
+dataPattern = re.compile(r"""^(.*?) # all text before the date
+    ((0|1)?\d)-# one or two digits for the month
+    ((0|1|2|3)?\d-# one or two digits for the day
+    ((19|20)\d\d)# four digits for the year
+    (.*?)$# all text after the date
+    """, re.VERBOSE)
+
+for amerFilename in os.listdir():
+    mo = dataPattern(amerFilename)
+
+    # Skip files without a date.
+    if mo == None:
+        continue
+
+    # Get the different parts of the filename.
+    beforePart = mo.group(1)
+    monthPart  = mo.group(2)
+    dayPart    = mo.group(4)
+    yearPart   = mo.group(6)
+    afterPart  = mo.group(8)
+
+    # Form the European-style filename
+    euroFilename = beforePart + dayPart + '-' + monthPart + '-' + yearPart + afterPart
+
+    # Get the full, absolute file paths.
+    absWorkingDir = os.path.abspath('.')
+    amerFilename = os.path.join(absWorkingDir, amerFilename)
+    euroFilename = os.path.join(absWorkingDir, euroFilename)
+
+    # Rename the files.
+    print('Renaming "%s" to "%s"...' %  (amerFilename, euroFilename))
+    ##shutil.move(amerFilename, euroFilename) # uncomment after testing
+    
